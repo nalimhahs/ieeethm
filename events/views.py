9690c6122961479b404a0fbf695b2db7d4bb23ca
Milan Shah <milanshah1@gmail.com>
2019-09-10 11:27:05 +0530

    Cosmetic changes and fixed broken links

--- events/views.py
+++ events/views.py
@@ -10,7 +10,7 @@ def allCatagories(request):
 
 def catagoryListing(request, catagory):
   events = Event.objects.filter(catagory__name=catagory)
-  return render(request, 'events/eventlist.html', { 'events': events })
+  return render(request, 'events/eventlist.html', { 'events': events, 'catagory': catagory })
 
 def eventListing(request, catagory, event_id):
   event = Event.objects.filter(event_id=event_id)
