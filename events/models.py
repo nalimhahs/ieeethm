0be6eac295416449fc67bdaf33cc560b8cb339fd
Milan Shah <milanshah1@gmail.com>
2019-09-13 09:17:28 +0530

    Converted some fields to optional and dynamically choosing to render them

--- events/models.py
+++ events/models.py
@@ -23,14 +23,14 @@ class Event(models.Model):
   event_id = models.DecimalField(max_digits=3, primary_key=True, decimal_places=0)
   name = models.CharField(max_length=64)
   desc = models.TextField(max_length=1024)
-  price = models.DecimalField(max_digits=10, decimal_places=2)
-  organizer_1 = models.ForeignKey(Organizer, on_delete=models.PROTECT, related_name='event_organizer_1')
-  organizer_2 = models.ForeignKey(Organizer, on_delete=models.PROTECT, related_name='event_organizer_2')
-  event_start = models.DateTimeField()
-  event_end = models.DateTimeField()
-  banner_url = models.URLField()
-  catagory = models.ForeignKey(Catagory, on_delete=models.PROTECT)
-  card_image_url = models.URLField()
+  price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+  organizer_1 = models.ForeignKey(Organizer, on_delete=models.PROTECT, related_name='event_organizer_1', null=True, blank=True)
+  organizer_2 = models.ForeignKey(Organizer, on_delete=models.PROTECT, related_name='event_organizer_2', null=True, blank=True)
+  event_start = models.DateTimeField(null=True, blank=True)
+  event_end = models.DateTimeField(null=True, blank=True)
+  banner_url = models.URLField(null=True, blank=True)
+  catagory = models.ForeignKey(Catagory, on_delete=models.PROTECT, null=True, blank=True)
+  reg_link = models.URLField(null=True, blank=True)
 
   def __str__(self):
     return self.name
